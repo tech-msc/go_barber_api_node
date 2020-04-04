9f308465bf9398a7494f1ae90ad6af866070481f
Matheus S. Cardoso <matheusscardoso5@github.com>
2020-04-04 01:30:23 -0300

    hash password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,27 @@
 import Sequelize, { Model } from 'sequelize'
+import Bcrypt from 'bcrypt'
+const SALT = 12
 
 class User extends Model {
   static init (sequelize) {
     super.init({
       name: Sequelize.STRING,
       email: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
       password_hash: Sequelize.STRING,
       provider: Sequelize.BOOLEAN
     },
     {
       sequelize
     })
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await Bcrypt.hash(user.password, SALT)
+      }
+    })
+
+    return this
   }
 }
 

38198eef4096febd6b7c274b4ae2c9775dd04d3b
tech-msc <matheusscardoso5@github.com>
2020-04-14 17:09:49 -0300

    list appointments by user

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,7 +2,36 @@ import Appointment from '../models/Appointment'
 import * as Yup from 'yup'
 import { startOfHour, parseISO, isBefore } from 'date-fns'
 import User from '../models/User'
+import File from '../models/File'
+
 class AppointmentController {
+  async index (req, res) {
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url']
+            }
+          ]
+        }
+      ]
+    })
+    // return res.json({ ok: 'okk' })
+    return res.json(appointments)
+  }
+
   async store (req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -55,7 +84,7 @@ class AppointmentController {
     }
 
     const appointment = await Appointment.create({
-      user_id: res.userId,
+      user_id: req.userId,
       provider_id,
       date: hourStart
     })
--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,7 @@ routes.post('/files', upload.single('file'), FileController.store)
 // eslint-disable-next-line no-undef
 routes.get('/providers', ProviderController.index)
 
+routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
 
 export default routes
